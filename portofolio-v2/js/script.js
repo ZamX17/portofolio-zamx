8eb7c2514800a42296905ec34555eb5e1b78369e
ZamX17 <zamzammio12o@gmail.com>
2025-05-19 10:13:33 +0700

    Add files via upload

---
+++ portofolio-v2/js/script.js
@@ -0,0 +1,49 @@
+// Toggle Class Active
+const navbarNav = document.querySelector(".navbar-nav");
+// Ketika Hamburger Menu di Klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.targer)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+ // Smooth scroll for internal anchor links
+ document.addEventListener('DOMContentLoaded', () => {
+  const links = document.querySelectorAll('a[href^="#"]');
+  links.forEach(link => {
+    link.addEventListener('click', e => {
+      const targetId = link.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+      if (targetElement) {
+        e.preventDefault();
+        targetElement.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+      }
+    });
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const observerOptions = {
+    threshold: 0.1
+  };
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if(entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, observerOptions);
+  document.querySelectorAll('.animate-on-scroll').forEach(elem => {
+    observer.observe(elem);
+  });
+});
